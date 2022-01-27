edc732c1284e3f7e1b42b6a4412135a38cb1c191
Casper Beyer <caspervonb@pm.me>
2022-01-28 05:59:10 +0800

    Add support for optional durations

--- src/de.rs
+++ src/de.rs
@@ -1,6 +1,10 @@
+use std::fmt;
+use std::marker::PhantomData;
+
 #[cfg(feature = "chrono")]
 use chrono;
 
+use serde::de::{Error, Visitor};
 use serde::Deserializer;
 
 /// Types that can be deserialized via `#[serde(with = "serde_nanos")]`.
@@ -21,6 +25,53 @@ impl<'de> Deserialize<'de> for std::time::Duration {
     }
 }
 
+impl<'de> Deserialize<'de> for Option<std::time::Duration> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct OptionVisitor<T> {
+            marker: PhantomData<T>,
+        }
+
+        impl<'de, T: Deserialize<'de>> Visitor<'de> for OptionVisitor<T> {
+            type Value = Option<T>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("option")
+            }
+
+            #[inline]
+            fn visit_unit<E>(self) -> Result<Option<T>, E>
+            where
+                E: Error,
+            {
+                Ok(None)
+            }
+
+            #[inline]
+            fn visit_none<E>(self) -> Result<Option<T>, E>
+            where
+                E: Error,
+            {
+                Ok(None)
+            }
+
+            #[inline]
+            fn visit_some<D>(self, deserializer: D) -> Result<Option<T>, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                T::deserialize(deserializer).map(Some)
+            }
+        }
+
+        deserializer.deserialize_option(OptionVisitor {
+            marker: PhantomData,
+        })
+    }
+}
+
 #[cfg(feature = "chrono")]
 impl<'de> Deserialize<'de> for chrono::Duration {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -29,5 +80,53 @@ impl<'de> Deserialize<'de> for chrono::Duration {
     {
         serde::Deserialize::deserialize(deserializer)
             .map(|nanos| chrono::Duration::nanoseconds(nanos))
+    }
+}
+
+#[cfg(feature = "chrono")]
+impl<'de> Deserialize<'de> for Option<chrono::Duration> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct OptionVisitor<T> {
+            marker: PhantomData<T>,
+        }
+
+        impl<'de, T: Deserialize<'de>> Visitor<'de> for OptionVisitor<T> {
+            type Value = Option<T>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("option")
+            }
+
+            #[inline]
+            fn visit_unit<E>(self) -> Result<Option<T>, E>
+            where
+                E: Error,
+            {
+                Ok(None)
+            }
+
+            #[inline]
+            fn visit_none<E>(self) -> Result<Option<T>, E>
+            where
+                E: Error,
+            {
+                Ok(None)
+            }
+
+            #[inline]
+            fn visit_some<D>(self, deserializer: D) -> Result<Option<T>, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                T::deserialize(deserializer).map(Some)
+            }
+        }
+
+        deserializer.deserialize_option(OptionVisitor {
+            marker: PhantomData,
+        })
     }
 }
